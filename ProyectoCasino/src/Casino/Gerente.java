06bc4e2def5d52b5d26c078f8858a367feb4c6e7
samuel-arango <samuel-arango@samuelarango-Lenovo-YOGA-510-14ISK>
2019-03-10 12:26:29 -0500

    Gerente

--- ProyectoCasino/src/Casino/Gerente.java
+++ ProyectoCasino/src/Casino/Gerente.java
@@ -1,5 +1,49 @@
 package Casino;
 
-public class Gerente {
+import java.io.*;
 
+public class Gerente {
+	
+	public static void leer(String ruta) {
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			
+			archivo = new File (ruta);
+			fr = new FileReader (archivo);
+			br = new BufferedReader(fr);
+			double cantidad2;
+			String linea;
+			String cantidad="";
+			String[] partes;
+			int i=0;
+			
+			while((linea=br.readLine())!=null && i<linea.length()) {
+				
+				String signo="";
+				partes=linea.split(" ");
+				if(partes[1].charAt(0)=='-') 
+					signo="-";
+					
+				cantidad=partes[1];
+				
+				cantidad2=Double.parseDouble(cantidad);
+				
+				System.out.println(cantidad2);
+			}
+			
+		} catch(IOException e){
+			System.out.println("No se pudo leer el archivo");;
+		}finally{
+			
+			try{                    
+				fr.close();
+				br.close();
+			}catch (IOException e2){ 
+				System.out.println("No se pudo cerrar el fichero");
+			}
+		}
+	}
 }
