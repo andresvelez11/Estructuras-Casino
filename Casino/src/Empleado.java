53e739ff9cc28396d360548dc572eb8405690b77
carit <carit@192.168.1.15>
2019-03-10 10:21:18 -0500

    dealer,gerente y empleado

--- Casino/src/Empleado.java
+++ Casino/src/Empleado.java
@@ -1,4 +1,14 @@
 
-public class Empleado {
+public  abstract class Empleado {
+protected float salario;
+protected String rango;
+
+abstract public String asignarRango();
+
+public Empleado(float salario, String rango) {
+	super();
+	this.salario = salario;
+	this.rango = rango;
+}
 
 }
--- Casino/src/Gerente.java
+++ Casino/src/Gerente.java
@@ -1,4 +1,34 @@
+import java.io.*;
+
 
 public class Gerente {
+	
+	public void leer(String ruta) {
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
 
+		try {
+			
+			archivo = new File (ruta);
+			fr = new FileReader (archivo);
+			br = new BufferedReader(fr);
+
+			String linea;
+			while((linea=br.readLine())!=null)
+				System.out.println(linea);
+		}
+		catch(IOException e){
+		System.out.println(e.getMessage());;
+		}finally{
+			
+			try{                    
+				if( null != fr ){   
+					fr.close();     
+				}                  
+			}catch (IOException e2){ 
+				System.out.println(e2.getMessage());
+			}
+		}
+	}
 }
